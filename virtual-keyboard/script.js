b2ef9c3d17b9aed8ba37df526464e8e20335b936
roman.mits <roman.mitskevich1@mail.ru>
2022-05-08 15:47:55 +0300

    feat: add highlighting buttons, change language and click events

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -1,5 +1,7 @@
 const documentBody = document.body;
 
+let currentLanguage = 'en';
+
 const addButtonIcon = (name) => {
    return `<i class="material-icons">${name}</i>`;
 };
@@ -11,8 +13,8 @@ documentBody.append(divWrapper);
 const wrapperHeading = `<h1>RSS Virtual keyboard</h1>`;
 divWrapper.insertAdjacentHTML('beforeend', wrapperHeading);
 
-const wrapperTextArea = `<textarea autofocus></textarea>`;
-divWrapper.insertAdjacentHTML('beforeend', wrapperTextArea);
+const wrapperTextArea = document.createElement('textarea');
+divWrapper.append(wrapperTextArea);
 
 const buttonsContainer = document.createElement('div');
 buttonsContainer.classList.add('buttons-container');
@@ -25,15 +27,27 @@ const changeLangText = `<p>Для переключения языка комби
 divWrapper.insertAdjacentHTML('beforeend', changeLangText);
 
 const russianButtons = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\'', 'Del',
+   'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del',
    'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
    'ShiftLeft', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ArrowUp', 'ShiftRight',
    'ControlLeft', 'Meta', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 
+const russianButtonsWithShift = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace',
+   'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\/', 'Del',
+   'CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter',
+   'ShiftLeft', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'ArrowUp', 'ShiftRight',
+   'ControlLeft', 'Meta', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+
 const englishButtons = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-   'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\'', 'Del',
+   'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del',
    'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter',
    'ShiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'ShiftRight',
+   'ControlLeft', 'Meta', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
+
+const englishButtonsWithShift = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace',
+   'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\|', 'Del',
+   'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter',
+   'ShiftLeft', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'ArrowUp', 'ShiftRight',
    'ControlLeft', 'Meta', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 
 
@@ -97,17 +111,80 @@ function createButtons(array) {
    });
 }
 
-createButtons(russianButtons);
+createButtons(englishButtons);
 
-buttonsContainer.addEventListener('keydown', (event) => {
+function deleteAllButtons() {
    let buttons = document.querySelectorAll('.keyboard-button');
-   if (event.target.classList.contains('keyboard-button')) {
-      buttons.forEach(element => {
-         console.log(event.code, element)
-         if (element.code === event.code) {
-            element.classList.add('active-button');
-         }
-      })
-   }
-})
+   buttons.forEach(element => element.remove());
+}
 
+window.addEventListener('keydown', (event) => {
+   if (event.ctrlKey && event.shiftKey) {
+      if (currentLanguage === 'en') {
+         deleteAllButtons();
+         currentLanguage = 'ru';
+         createButtons(russianButtons);
+      } else if (currentLanguage === 'ru') {
+         deleteAllButtons();
+         currentLanguage = 'en';
+         createButtons(englishButtons);
+      }
+   }
+});
+
+window.addEventListener('keydown', (event) => {
+   if (event.shiftKey) {
+      if (currentLanguage === 'ru') {
+         deleteAllButtons();
+         createButtons(russianButtonsWithShift);
+      } else if (currentLanguage === 'en') {
+         deleteAllButtons();
+         createButtons(englishButtonsWithShift);
+      }
+   }
+});
+
+window.addEventListener('keyup', (event) => {
+   if (event.key === 'Shift') {
+      if (currentLanguage === 'ru') {
+         deleteAllButtons();
+         createButtons(russianButtons);
+      } else if (currentLanguage === 'en') {
+         deleteAllButtons();
+         createButtons(englishButtons);
+      }
+   }
+});
+
+window.addEventListener('keydown', (event) => {
+
+
+   let buttons = document.querySelectorAll('.keyboard-button');
+   buttons.forEach(element => {
+      if (event.key === element.textContent) {
+         element.classList.add('active-button');
+         wrapperTextArea.textContent += element.textContent;
+      }
+   })
+   if (event.shiftKey && event.code === 'ShiftLeft') {
+      console.log(true)
+      event.key.classList.add('active-button');
+   } else if (event.shiftKey && event.code === 'ShiftRight') {
+      document.querySelector('.shift-right-button').classList.add('active-button');
+   }
+});
+
+window.addEventListener('keyup', (event) => {
+   let buttons = document.querySelectorAll('.keyboard-button');
+   buttons.forEach(element => {
+      if (event.key === element.textContent) {
+         element.classList.remove('active-button');
+      }
+   })
+});
+
+buttonsContainer.addEventListener("click", (event) => {
+   if (event.target.classList.contains('keyboard-button')) {
+      wrapperTextArea.textContent += event.target.textContent;
+   }
+});
